037907273c63eb58b4334a5fd6bc6c236b86fc5a
welton816118698 <42454946+welton816118698@users.noreply.github.com>
2018-08-17 22:17:02 -0300

    Add files via upload

---
+++ Aula 2 Codigos.R
@@ -0,0 +1,5 @@
+rep(10,5)                      #Repete o 10 cinco vezes
+install.packages("pwt8.0")     #Instalar o pacote PWT8.0
+rm(list = "x", "y")           #Remove variáveis
+setwd("c:/econometria")        #Para setar a pasta de econometria como local para salver
+getwd()                        #Para verificar qual pasta setamos
